c8f2dc3753af256695fc9b37707efee80c4b92e0
IMRANDIL <aliimranadil2@gmail.com>
2024-03-16 19:17:36 +0530

    added some more code

--- password_manager/password_manager.py
+++ password_manager/password_manager.py
@@ -2,11 +2,20 @@
 
 pwd = input("What is the master password? ")
 
-mode = input("Would you like to add a new password or view existing ones (view, add)? ").lower()
 
-if mode == "view":
-    pass
-elif mode == "add":
-    pass
-else:
-    print("Sorry, You have to choose either add or view, try again")
+
+
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+
+    if mode == 'q':
+        print("Bye!")
+        quit()
+        
+    if mode == "view":
+        pass
+    elif mode == "add":
+        pass
+    else:
+        print("Sorry, You have to choose either add or view, try again")
