fb7b14ebcdca8c971b62dd05960953da78560c33
IMRANDIL <aliimranadil2@gmail.com>
2024-03-16 18:41:15 +0530

    adding logic for the adventure

--- choose_your_own_adventure/choose_adventure.py
+++ choose_your_own_adventure/choose_adventure.py
@@ -5,3 +5,15 @@ print(f"Welcome {name} to this adventure!")
 
 answer = input(f"{name} you are on a dirt road, it has come to an end and you can go left or right, which way you want to go? ").lower()
 
+if answer == 'left':
+    answer = input(f"You come to a river, you can walk around it or swim across? Type walk to walk around and swim to swim across: ").lower()
+    if answer == 'walk':
+        print(f"")
+    else:
+        print('')
+    
+elif answer == 'right':
+    print('hi')
+    
+else:
+    print(f"Invalid option {name}, You lost!")
