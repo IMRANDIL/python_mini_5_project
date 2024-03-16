515015dbce6b873330fc80eaf082611561c416f1
IMRANDIL <aliimranadil2@gmail.com>
2024-03-16 09:49:40 +0530

    added initial program for quiz ap

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -2,4 +2,16 @@ from loadJson import require_json
 
 # Usage example
 quiz_data = require_json('quiz_data.json')
-print(quiz_data)
+score_count = 0
+print("Let's Play the Quiz")
+for quest_data in quiz_data:
+    quest_answer = input(quest_data['Question']).lower()
+    if(quest_answer == quest_data['Answer'].lower()):
+        score_count += quest_data['Points']
+    print(quest_data)
+
+print(f'You Scored {score_count}.\n Thank You')
+
+# print(quiz_data)
+
+print(len(quiz_data))
