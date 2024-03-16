519d65a121e34d6c9982881339e8f37a474a3079
IMRANDIL <aliimranadil2@gmail.com>
2024-03-16 14:30:39 +0530

    rock paper scissors

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -2,11 +2,34 @@ import random
 
 user_wins = 0
 computer_wins = 0
-
+choices = ['Rock', 'Paper', 'Scissors']
 while True:
     user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     if user_input == "q":
         print("Bye")
         quit()
+    computer_choice = choices[random.randint(0, len(choices))].lower()
+    if user_input in choices:
+        if user_input == 'Rock'.lower() and computer_choice == 'Paper'.lower():
+            print('You won!')
+            user_wins += 1
+            continue
+        if user_input == 'Scissors'.lower() and computer_choice == 'Paper'.lower():
+            print('You won!')
+            user_wins += 1
+            continue
+        
+        
+        
+        
+    
+    
+    else:
+        print("Please choose amongst 'Rock' or 'Paper' or 'Scissors'")
+        continue
+    
+       
+    
+    
     
     
