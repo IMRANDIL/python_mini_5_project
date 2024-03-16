b089c594cc514452bff342fec48c6fcd32b9c82e
IMRANDIL <aliimranadil2@gmail.com>
2024-03-16 09:53:17 +0530

    basic python quiz is ready

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -5,12 +5,13 @@ quiz_data = require_json('quiz_data.json')
 score_count = 0
 print("Let's Play the Quiz")
 for quest_data in quiz_data:
-    quest_answer = input(quest_data['Question']).lower()
+    quest_answer = input(f'{quest_data['Question']} ').lower()
     if(quest_answer == quest_data['Answer'].lower()):
         score_count += quest_data['Points']
-    print(quest_data)
+    else:
+        continue
 
-print(f'You Scored {score_count}.\n Thank You')
+print(f'You Scored {score_count}.\nThank You')
 
 # print(quiz_data)
 
