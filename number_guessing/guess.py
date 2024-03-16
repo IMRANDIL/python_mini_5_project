95665311bfd7eadc40dca208d277f2bcd0a97c4f
IMRANDIL <aliimranadil2@gmail.com>
2024-03-16 13:56:31 +0530

    some more check in place

--- number_guessing/guess.py
+++ number_guessing/guess.py
@@ -22,21 +22,25 @@ while True:
     if guessing_number == 'q':
         print('Bye')
         quit()
-    if int(guessing_number) > top_of_range:
-        print(f"Please guess a number within the range '0' to '{top_of_range}' you provided earlier")
-        continue
-    attempts_no += 1
-    if int(guessing_number) > random_number:
-        print(f"The number is greater, try again")
-        continue
-    if int(guessing_number) < random_number:
-        print(f"The number is lower, try again")
-        continue
-    
-    if random_number == int(guessing_number):
-        print("You got it")
-        break
+    if guessing_number.isdigit():
+        if int(guessing_number) > top_of_range:
+            print(f"Please guess a number within the range '0' to '{top_of_range}' you provided earlier")
+            continue
+        attempts_no += 1
+        if int(guessing_number) > random_number:
+            print(f"The number is greater, try again")
+            continue
+        if int(guessing_number) < random_number:
+            print(f"The number is lower, try again")
+            continue
+        
+        if random_number == int(guessing_number):
+            print("You got it")
+            break
+        else:
+            continue
     else:
+        print('Please type a number next time.')
         continue
     
     
