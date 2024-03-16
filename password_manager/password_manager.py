ae6d6e357a7fd4dcfd4a772ca7408952a20780c7
IMRANDIL <aliimranadil2@gmail.com>
2024-03-16 19:15:25 +0530

    added password manager

---
+++ password_manager/password_manager.py
@@ -0,0 +1,12 @@
+# just a fun project..can't be implemented in production
+
+pwd = input("What is the master password? ")
+
+mode = input("Would you like to add a new password or view existing ones (view, add)? ").lower()
+
+if mode == "view":
+    pass
+elif mode == "add":
+    pass
+else:
+    print("Sorry, You have to choose either add or view, try again")
