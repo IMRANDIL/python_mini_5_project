d7aa1c980ad29fd855fcf975c680cf3f12b7fa66
IMRANDIL <aliimranadil2@gmail.com>
2024-03-16 18:36:12 +0530

    choose adventure game python

---
+++ choose_your_own_adventure/choose_adventure.py
@@ -0,0 +1,7 @@
+name = input("Type your name: ")
+
+print(f"Welcome {name} to this adventure!")
+
+
+answer = input(f"{name} you are on a dirt road, it has come to an end and you can go left or right, which way you want to go? ").lower()
+
