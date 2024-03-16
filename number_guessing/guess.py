99255534f7f4fcc85cd5b575c315ac63a0a816b6
IMRANDIL <aliimranadil2@gmail.com>
2024-03-16 12:51:29 +0530

    guessing game python, loved it

--- number_guessing/guess.py
+++ number_guessing/guess.py
@@ -1,6 +1,8 @@
 import random
 
-top_of_range = input("Type a number: ")
+top_of_range = input("Type a number range: ")
+
+attempts_no = 0
 
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
@@ -11,3 +13,32 @@ if top_of_range.isdigit():
 else:
     print("Please type a number next time.")
     quit()
+    
+random_number = random.randint(0, top_of_range)
+
+
+while True:
+    guessing_number = input('Please guess a number between the range (press "q" to exit) ')
+    if guessing_number == 'q':
+        print('Bye')
+        quit()
+    if int(guessing_number) > top_of_range:
+        print(f"Please guess a number within the range '0' to '{top_of_range}' you provided earlier")
+        continue
+    attempts_no += 1
+    if int(guessing_number) > random_number:
+        print(f"The number is greater, try again")
+        continue
+    if int(guessing_number) < random_number:
+        print(f"The number is lower, try again")
+        continue
+    
+    if random_number == int(guessing_number):
+        print("You got it")
+        break
+    else:
+        continue
+    
+    
+print(f"You got it in {attempts_no} {'attempts' if attempts_no > 1 else 'attempt'}.")
+    
